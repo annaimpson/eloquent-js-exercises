cea1ca31c549ccd3e4b9fe6579e6d27e9385d514
Anna Impson <annaimpson@gmail.com>
2017-05-09 09:24:49 -0400

    chapter 10 exercise 1

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -708,3 +708,31 @@ withBoxUnlocked(function() {
   throw new Error("Pirates on the horizon! Abort!");
 });
 console.log(box.locked);
+
+
+
+
+
+//Chapter 10
+
+
+
+
+//Write a simple module similar to the weekDay module that can convert
+// month numbers (zero-based, as in the Date type) to names and can convert
+// names back to numbers. Give it its own namespace since it will need
+// an internal array of month names, and use plain JavaScript, without any
+// module loader system.
+var month = function() {
+  var names = ["January", "February", "March", "April", "May",
+               "June", "July", "August", "September", "October",
+               "November", "December"];
+  return {
+    name: function(number) { return names[number]; },
+    number: function(name) { return names.indexOf(name); }
+  };
+}();
+
+
+console.log(month.name(7));
+console.log(month.number("September"));
