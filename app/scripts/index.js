225200f2650d608cb1e7e33d79641d5033961f1b
Anna Impson <annaimpson@gmail.com>
2017-05-18 09:37:35 -0400

    chapter 11

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -736,3 +736,82 @@ var month = function() {
 
 console.log(month.name(7));
 console.log(month.number("September"));
+
+
+
+
+
+
+//Chapter 11
+
+
+
+
+
+// Add support for arrays to Egg by adding the following three functions to
+// the top scope: array(...) to construct an array containing the argument
+// values, length(array) to get an array’s length, and element(array, n) to
+// fetch the nth element from an array.
+topEnv["array"] = function() {
+  return Array.prototype.slice.call(arguments, 0);
+};
+
+topEnv["length"] = function(array) {
+  return array.length;
+};
+
+topEnv["element"] = function(array, i) {
+  return array[i];
+};
+
+run("do(define(sum, fun(array,",
+    "     do(define(i, 0),",
+    "        define(sum, 0),",
+    "        while(<(i, length(array)),",
+    "          do(define(sum, +(sum, element(array, i))),",
+    "             define(i, +(i, 1)))),",
+    "        sum))),",
+    "   print(sum(array(1, 2, 3))))");
+
+
+
+
+
+// It would be nice if we could write comments in Egg. For example, whenever
+// we find a hash sign (\#), we could treat the rest of the line as a
+// comment and ignore it, similar to // in JavaScript.
+
+function skipSpace(string) {
+  var skippable = string.match(/^(\s|#.*)*/);
+  return string.slice(skippable[0].length);
+}
+
+console.log(parse("# hello\nx"));
+console.log(parse("a # one\n   # two\n()"));
+
+
+
+
+
+//fixing scope
+
+specialForms["set"] = function(args, env) {
+  if (args.length != 2 || args[0].type != "word")
+    throw new SyntaxError("Bad use of set");
+  var varName = args[0].name;
+  var value = evaluate(args[1], env);
+
+  for (var scope = env; scope; scope = Object.getPrototypeOf(scope)) {
+    if (Object.prototype.hasOwnProperty.call(scope, varName)) {
+      scope[varName] = value;
+      return value;
+    }
+  }
+  throw new ReferenceError("Setting undefined variable " + varName);
+};
+
+run("do(define(x, 4),",
+    "   define(setx, fun(val, set(x, val))),",
+    "   setx(50),",
+    "   print(x))");
+run("set(quux, true)");
